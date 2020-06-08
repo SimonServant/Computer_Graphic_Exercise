4ad54cda473359eb50196123f3299fa63257ad26
Simon Diener <simon@fam-diener.de>
2020-06-08 10:53:57 +0200

    First try error

--- src/phong.ts
+++ src/phong.ts
@@ -30,20 +30,26 @@ export default function phong(
   // TODO: n = ? Is n Normal Vector of intersection ? No normally not. N Should be vektor from center of Orb to Intersection Point but we got no center here therefore we might have to change stuff
 
   // s == (P=>S) = S - P
-  var s = lightPositions[0].sub(intersection.point);
-  // r == s gespiegelt an n => r=d−2(d⋅n)n // r = 
-  var r =  s.sub(intersection.normal.mul(s.dot(intersection.normal)).mul(2));
+  var s = lightPositions[0].sub(intersection.point).normalised();
+  // // r == s gespiegelt an n => r=d−2(d⋅n)n // r = 
+  // var r =  s.sub(intersection.normal.mul(s.dot(intersection.normal)).mul(2));
+
+  var r = intersection.normal.mul(s.dot(intersection.normal)).mul(2).sub(s)
   // Last vektor is V towards the eye therefore =>
-  var v = cameraPosition.sub(intersection.point);
+  var v = cameraPosition.sub(intersection.point).normalised();
 
   // Ambient Lighting: Lr  =  ka L^a | Right now missing Lr Reflected ambient engery and La fictitious ambient light energy
+  var Lr = lightColor.mul(kA);
 
   // Diffuse Lighting: Ld(ps= Lj max(0, n.dot(s) => Missing Lj = Li(p,wi))
+  var Ld = lightColor.mul(Math.max(0,intersection.normal.dot(s))).mul(kD);
 
   // Specular Reflection: ks Sum over j Lj max(9, r_j.dot(v)^ke)
+  var Ls = lightColor.mul(Math.pow(Math.max(0,r.dot(v)), kE)).mul(kS);
 
   // Tipps implementierung: Normalisierung aller vektoren, Lichtquelle nicht verwenden wenn hinter Surface => n dot s kleiner gleich 0 dann ist Lj auch null
 
   // TODO [exercise 6]
+  // var color = Lr.add(Ld).add(Ls);
   return color;
 }
