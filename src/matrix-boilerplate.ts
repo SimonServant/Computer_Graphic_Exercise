ea649f84ee0002cd6e496d8620db7c52044ba31f
Yousof Shehada <yousof.shehada@stud-mail.uni-wuerzburg.de>
2020-06-15 10:51:26 +0200

    Exercise 7

---
+++ src/matrix-boilerplate.ts
@@ -0,0 +1,142 @@
+import 'bootstrap';
+import 'bootstrap/scss/bootstrap.scss';
+import Intersection from './intersection';
+import Ray from './ray';
+import phong from './phong';
+import Sphere from './sphere';
+import Vector from './vector';
+import Matrix from './matrix';
+
+window.addEventListener('load', () => {
+    const canvas = document.getElementById("raytracer") as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d");
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+    const objects = [
+        new Sphere(new Vector(.5, -.2, -2, 1), 0.4, new Vector(.3, 0, 0, 1)),
+        new Sphere(new Vector(-.5, -.2, -1.5, 1), 0.4, new Vector(0, 0, .3, 1))
+    ];
+    const lightPositions = [
+        new Vector(1, 1, -1, 1)
+    ];
+    const shininess = 10;
+    const camera = {
+        origin: new Vector(0, 0, 0, 1),
+        width: canvas.width,
+        height: canvas.height,
+        alpha: Math.PI / 3
+    }
+
+    function setPixel(x: number, y: number, color: Vector) {
+        data[4 * (canvas.width * y + x) + 0] = Math.min(255, color.r * 255);
+        data[4 * (canvas.width * y + x) + 1] = Math.min(255, color.g * 255);
+        data[4 * (canvas.width * y + x) + 2] = Math.min(255, color.b * 255);
+        data[4 * (canvas.width * y + x) + 3] = Math.min(255, color.a * 255);
+    }
+
+    let rotation = Matrix.identity();
+    let translation = Matrix.identity();
+    let scale = Matrix.identity();
+
+    function animate() {
+        data.fill(0);
+        let manipulatedObjects = [];
+        for (let obj of objects) {
+            manipulatedObjects.push(
+                new Sphere(
+                    rotation.mul(scale).mul(translation).mul(obj.center),
+                    obj.radius, obj.color
+                )
+            );
+        }
+        for (let x = 0; x < canvas.width; x++) {
+            for (let y = 0; y < canvas.height; y++) {
+                const ray = Ray.makeRay(x, y, camera);
+                let minIntersection = new Intersection(Infinity, null, null);
+                let minObj = null;
+                for (let shape of manipulatedObjects) {
+                    const intersection = shape.intersect(ray);
+                    if (intersection && intersection.closerThan(minIntersection)) {
+                        minIntersection = intersection;
+                        minObj = shape;
+                    }
+                }
+                if (minObj) {
+                    if (!minObj.color) {
+                        setPixel(x, y, new Vector(0, 0, 0, 1));
+                    } else {
+                        const color = phong(minObj.color, minIntersection, lightPositions, shininess, camera.origin);
+                        setPixel(x, y, color);
+                    }
+
+                }
+            }
+        }
+
+        ctx.putImageData(imageData, 0, 0);
+    }
+    window.requestAnimationFrame(animate);
+
+    const useRotationElement = document.getElementById("userotation") as HTMLInputElement;
+    useRotationElement.onchange = () => {
+        let range = document.getElementById("rotation") as HTMLInputElement;
+        if (useRotationElement.checked) {
+            range.oninput = () => {
+                rotation = Matrix.rotation(new Vector(0, 0, 1, 0),
+                    Number(range.value));
+                window.requestAnimationFrame(animate);
+            }
+            range.disabled = false;
+            range.oninput(new Event("click"));
+        } else {
+            range.disabled = true;
+            rotation = Matrix.identity();
+        }
+        window.requestAnimationFrame(animate);
+    }
+
+    const useTranslationElement = document.getElementById("usetranslation") as HTMLInputElement;
+    useTranslationElement.onchange = () => {
+        let range = document.getElementById("translation") as HTMLInputElement;
+        if (useTranslationElement.checked) {
+            range.oninput = () => {
+                translation = Matrix.translation(new Vector(Number(range.value), 0, 0, 0));
+                window.requestAnimationFrame(animate);
+            }
+            range.disabled = false;
+            range.oninput(new Event("click"));
+        } else {
+            range.disabled = true;
+            translation = Matrix.identity();
+        }
+        window.requestAnimationFrame(animate);
+    }
+
+    const useScaleElement = document.getElementById("usescale") as HTMLInputElement;
+    useScaleElement.onchange = () => {
+        let range = document.getElementById("scale") as HTMLInputElement;
+        if (useScaleElement.checked) {
+            range.oninput = () => {
+                scale = Matrix.scaling(new Vector(
+                    Number(range.value),
+                    Number(range.value),
+                    Number(range.value), 0));
+                window.requestAnimationFrame(animate);
+            }
+            range.disabled = false;
+            range.oninput(new Event("click"));
+        } else {
+            range.disabled = true;
+            scale = Matrix.identity();
+        }
+        window.requestAnimationFrame(animate);
+    }
+
+    const sliders = ["rotation", "translation", "scale"];
+    for (let t of sliders) {
+        const elem = document.getElementById("use" + t) as HTMLInputElement;
+        if (elem.checked) {
+            elem.onchange(new Event("click"));
+        }
+    }
+});
---
+++ src/matrix.ts
@@ -0,0 +1,268 @@
+import Vector from './vector';
+
+/**
+ * Class representing a 4x4 Matrix
+ */
+export default class Matrix {
+
+  /**
+   * Data representing the matrix values
+   */
+  data: Float32Array;
+
+  /**
+   * Constructor of the matrix. Expects an array in row-major layout. Saves the data as column major internally.
+   * @param mat Matrix values row major
+   */
+  constructor(mat: Array<number>) {
+    this.data = new Float32Array(16);
+    for (let row = 0; row < 4; row++) {
+      for (let col = 0; col < 4; col++) {
+        this.data[row * 4 + col] = mat[col * 4 + row];
+      }
+    }
+  }
+
+  /**
+   * Returns the value of the matrix at position row, col
+   * @param row The value's row
+   * @param col The value's column
+   * @return The requested value
+   */
+  getVal(row: number, col: number): number {
+    return this.data[col * 4 + row];
+  }
+
+  /**
+   * Sets the value of the matrix at position row, col
+   * @param row The value's row
+   * @param val The value to set to
+   * @param col The value's column
+   */
+  setVal(row: number, col: number, val: number) {
+    this.data[col * 4 + row] = val;
+  }
+
+  /**
+   * Returns a matrix that represents a translation
+   * @param translation The translation vector that shall be expressed by the matrix
+   * @return The resulting translation matrix
+   */
+  static translation(translation: Vector): Matrix {
+    // TODO exercise 7
+  }
+
+  /**
+   * Returns a matrix that represents a rotation. The rotation axis is either the x, y or z axis (either x, y, z is 1).
+   * @param axis The axis to rotate around
+   * @param angle The angle to rotate
+   * @return The resulting rotation matrix
+   */
+  static rotation(axis: Vector, angle: number): Matrix {
+    // TODO exercise 7
+  }
+
+  /**
+   * Returns a matrix that represents a scaling
+   * @param scale The amount to scale in each direction
+   * @return The resulting scaling matrix
+   */
+  static scaling(scale: Vector): Matrix {
+    // TODO exercise 7
+  }
+
+  /**
+   * Constructs a lookat matrix
+   * @param eye The position of the viewer
+   * @param center The position to look at
+   * @param up The up direction
+   * @return The resulting lookat matrix
+   */
+  static lookat(eye: Vector, center: Vector, up: Vector): Matrix {
+    // TODO exercise 10
+  }
+
+  /**
+   * Constructs a new matrix that represents a projection normalisation transformation
+   * @param left Camera-space left value of lower near point
+   * @param right Camera-space right value of upper right far point
+   * @param bottom Camera-space bottom value of lower lower near point
+   * @param top Camera-space top value of upper right far point
+   * @param near Camera-space near value of lower lower near point
+   * @param far Camera-space far value of upper right far point
+   * @return The rotation matrix
+   */
+  static frustum(left: number, right: number, bottom: number, top: number, near: number, far: number): Matrix {
+    // TODO exercise 11
+  }
+
+  /**
+   * Constructs a new matrix that represents a projection normalisation transformation.
+   * @param fovy Field of view in y-direction
+   * @param aspect Aspect ratio between width and height
+   * @param near Camera-space distance to near plane
+   * @param far Camera-space distance to far plane
+   * @return The resulting matrix
+   */
+  static perspective(fovy: number, aspect: number, near: number, far: number): Matrix {
+    // TODO exercise 11
+  }
+
+  /**
+   * Returns the identity matrix
+   * @return A new identity matrix
+   */
+  static identity(): Matrix {
+    return new Matrix([
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
+    ]);
+  }
+
+  /**
+   * Matrix multiplication
+   * @param other The matrix or vector to multiplicate with
+   * @return The result of the multiplication this*other
+   */
+  mul(other: Matrix): Matrix;
+  mul(other: Vector): Vector;
+  mul(other: Matrix | Vector): Matrix | Vector {
+    if (other instanceof Matrix) {
+      // TODO exercise 7
+    } else { // other is vector
+      // TODO exercise 7
+    }
+  }
+
+  /**
+   * Returns the transpose of this matrix
+   * @return A new matrix that is the transposed of this
+   */
+  transpose(): Matrix {
+    // TODO exercise 7
+  }
+
+  /**
+   * Debug print to console
+   */
+  print() {
+    for (let row = 0; row < 4; row++) {
+      console.log("> " + this.getVal(row, 0) +
+        "\t" + this.getVal(row, 1) +
+        "\t" + this.getVal(row, 2) +
+        "\t" + this.getVal(row, 3)
+      );
+    }
+  }
+
+  /**
+   * Returns a new matrix that is the inverse of this matrix
+   * @return The inverse matrix
+   */
+  invert(): Matrix {
+    let mat = this.data;
+    let dst = new Float32Array(16); //ret.getValues();
+    let tmp = new Float32Array(12);
+
+    /* temparray for pairs */
+    let src = new Float32Array(16); //new float[16];
+
+    /* array of transpose source matrix */
+    let det;
+
+    /* determinant */
+    /*
+     * transpose matrix
+     */
+    for (let i = 0; i < 4; i++) {
+      src[i] = mat[i * 4];
+      src[i + 4] = mat[i * 4 + 1];
+      src[i + 8] = mat[i * 4 + 2];
+      src[i + 12] = mat[i * 4 + 3];
+    }
+
+    /* calculate pairs for first 8 elements (cofactors) */
+    tmp[0] = src[10] * src[15];
+    tmp[1] = src[11] * src[14];
+    tmp[2] = src[9] * src[15];
+    tmp[3] = src[11] * src[13];
+    tmp[4] = src[9] * src[14];
+    tmp[5] = src[10] * src[13];
+    tmp[6] = src[8] * src[15];
+    tmp[7] = src[11] * src[12];
+    tmp[8] = src[8] * src[14];
+    tmp[9] = src[10] * src[12];
+    tmp[10] = src[8] * src[13];
+    tmp[11] = src[9] * src[12];
+
+    /* calculate first 8 elements (cofactors) */
+    dst[0] = tmp[0] * src[5] + tmp[3] * src[6] + tmp[4] * src[7];
+    dst[0] -= tmp[1] * src[5] + tmp[2] * src[6] + tmp[5] * src[7];
+    dst[1] = tmp[1] * src[4] + tmp[6] * src[6] + tmp[9] * src[7];
+    dst[1] -= tmp[0] * src[4] + tmp[7] * src[6] + tmp[8] * src[7];
+    dst[2] = tmp[2] * src[4] + tmp[7] * src[5] + tmp[10] * src[7];
+    dst[2] -= tmp[3] * src[4] + tmp[6] * src[5] + tmp[11] * src[7];
+    dst[3] = tmp[5] * src[4] + tmp[8] * src[5] + tmp[11] * src[6];
+    dst[3] -= tmp[4] * src[4] + tmp[9] * src[5] + tmp[10] * src[6];
+    dst[4] = tmp[1] * src[1] + tmp[2] * src[2] + tmp[5] * src[3];
+    dst[4] -= tmp[0] * src[1] + tmp[3] * src[2] + tmp[4] * src[3];
+    dst[5] = tmp[0] * src[0] + tmp[7] * src[2] + tmp[8] * src[3];
+    dst[5] -= tmp[1] * src[0] + tmp[6] * src[2] + tmp[9] * src[3];
+    dst[6] = tmp[3] * src[0] + tmp[6] * src[1] + tmp[11] * src[3];
+    dst[6] -= tmp[2] * src[0] + tmp[7] * src[1] + tmp[10] * src[3];
+    dst[7] = tmp[4] * src[0] + tmp[9] * src[1] + tmp[10] * src[2];
+    dst[7] -= tmp[5] * src[0] + tmp[8] * src[1] + tmp[11] * src[2];
+
+    /* calculate pairs for second 8 elements (cofactors) */
+    tmp[0] = src[2] * src[7];
+    tmp[1] = src[3] * src[6];
+    tmp[2] = src[1] * src[7];
+    tmp[3] = src[3] * src[5];
+    tmp[4] = src[1] * src[6];
+    tmp[5] = src[2] * src[5];
+    tmp[6] = src[0] * src[7];
+    tmp[7] = src[3] * src[4];
+    tmp[8] = src[0] * src[6];
+    tmp[9] = src[2] * src[4];
+    tmp[10] = src[0] * src[5];
+    tmp[11] = src[1] * src[4];
+
+    /* calculate second 8 elements (cofactors) */
+    dst[8] = tmp[0] * src[13] + tmp[3] * src[14] + tmp[4] * src[15];
+    dst[8] -= tmp[1] * src[13] + tmp[2] * src[14] + tmp[5] * src[15];
+    dst[9] = tmp[1] * src[12] + tmp[6] * src[14] + tmp[9] * src[15];
+    dst[9] -= tmp[0] * src[12] + tmp[7] * src[14] + tmp[8] * src[15];
+    dst[10] = tmp[2] * src[12] + tmp[7] * src[13] + tmp[10] * src[15];
+    dst[10] -= tmp[3] * src[12] + tmp[6] * src[13] + tmp[11] * src[15];
+    dst[11] = tmp[5] * src[12] + tmp[8] * src[13] + tmp[11] * src[14];
+    dst[11] -= tmp[4] * src[12] + tmp[9] * src[13] + tmp[10] * src[14];
+    dst[12] = tmp[2] * src[10] + tmp[5] * src[11] + tmp[1] * src[9];
+    dst[12] -= tmp[4] * src[11] + tmp[0] * src[9] + tmp[3] * src[10];
+    dst[13] = tmp[8] * src[11] + tmp[0] * src[8] + tmp[7] * src[10];
+    dst[13] -= tmp[6] * src[10] + tmp[9] * src[11] + tmp[1] * src[8];
+    dst[14] = tmp[6] * src[9] + tmp[11] * src[11] + tmp[3] * src[8];
+    dst[14] -= tmp[10] * src[11] + tmp[2] * src[8] + tmp[7] * src[9];
+    dst[15] = tmp[10] * src[10] + tmp[4] * src[8] + tmp[9] * src[9];
+    dst[15] -= tmp[8] * src[9] + tmp[11] * src[10] + tmp[5] * src[8];
+
+    /* calculate determinant */
+    det = src[0] * dst[0] + src[1] * dst[1] + src[2] * dst[2] + src[3] * dst[3];
+
+    if (det == 0.0) {
+      throw new Error("singular matrix is not invertible");
+    }
+
+    /* calculate matrix inverse */
+    det = 1 / det;
+
+    for (let j = 0; j < 16; j++) {
+      dst[j] *= det;
+    }
+
+    let ret = Matrix.identity();
+    ret.data = dst;
+    return ret;
+  }
+}
