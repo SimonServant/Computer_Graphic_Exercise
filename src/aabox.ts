d456581f436acbb3716456e98e5bb5fc0ffd802b
Simon Diener <simon@fam-diener.de>
2020-06-22 10:54:36 +0200

    Update exercise 8

---
+++ src/aabox.ts
@@ -0,0 +1,63 @@
+import Vector from './vector';
+import Ray from './ray';
+import Intersection from './intersection';
+
+/**
+ * Class representing an axis aligned box
+ */
+export default class AABox {
+  /**
+   * The box's vertices
+   */
+  vertices: Array<Vector>;
+  /**
+   * The indices of the vertices that
+   * together form the faces of the box
+   */
+  indices: Array<number>;
+
+  /**
+   * Creates an axis aligned box
+   * @param minPoint The minimum Point
+   * @param maxPoint The maximum Point
+   * @param color The colour of the cube
+   */
+  constructor(minPoint: Vector, maxPoint: Vector, public color: Vector) {
+    /*
+      7----6
+     /|   /|   2 = maxPoint
+    3----2 |   4 = minPoint
+    | 4--|-5   Looking into negative z direction
+    |/   |/
+    0----1
+     */
+    this.vertices = [
+      new Vector(minPoint.x, minPoint.y, maxPoint.z, 1),
+      new Vector(maxPoint.x, minPoint.y, maxPoint.z, 1),
+      new Vector(maxPoint.x, maxPoint.y, maxPoint.z, 1),
+      new Vector(minPoint.x, maxPoint.y, maxPoint.z, 1),
+      new Vector(minPoint.x, minPoint.y, minPoint.z, 1),
+      new Vector(maxPoint.x, minPoint.y, minPoint.z, 1),
+      new Vector(maxPoint.x, maxPoint.y, minPoint.z, 1),
+      new Vector(minPoint.x, maxPoint.y, minPoint.z, 1)
+    ];
+    this.indices = [
+      0, 1, 2, 3,
+      1, 5, 6, 2,
+      4, 0, 3, 7,
+      3, 2, 6, 7,
+      5, 4, 7, 6,
+      0, 4, 5, 1
+    ];
+  }
+
+  /**
+   * Calculates the intersection of the AAbox with the given ray
+   * @param ray The ray to intersect with
+   * @return The intersection if there is one, null if there is none
+   */
+  intersect(ray: Ray): Intersection | null {
+    // TODO [exercise 8 optional]
+  }
+
+}
